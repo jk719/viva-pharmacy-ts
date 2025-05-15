bbbbd6ab39c4c4d0a3ae2c8de607b9ecf8e97dad
Jamil Kabir <jamilkabir719@gmail.com>
2025-05-15 02:25:41 -0400

    style: unify quotes in jest.config.js for consistency

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ["<rootDir>/modules", "<rootDir>/core"],
-  moduleFileExtensions: ["ts", "js", "json"],
-  testMatch: ["**/?(*.)+(spec|test).ts"],
+  roots: ['<rootDir>/modules', '<rootDir>/core'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/?(*.)+(spec|test).ts'],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.json',
